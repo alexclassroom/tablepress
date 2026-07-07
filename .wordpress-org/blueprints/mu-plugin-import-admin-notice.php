4674f289c115c66238b8f2409214f0fcf5c0e1dd
TobiasBg <git@tobias.baethge.com>
2026-07-07 22:22:11 +0200

    Finish TablePress 3.3.3.

--- .wordpress-org/blueprints/mu-plugin-import-admin-notice.php
+++ .wordpress-org/blueprints/mu-plugin-import-admin-notice.php
@@ -8,16 +8,18 @@
  * @since 3.0.0
  */
 
+// Prohibit direct script loading.
+defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
+
 /*
  * Print a warning notice about the import from URLs likely not working for all URLs.
  * WordPress Playground tries getting the import URL via JavaScript, which is subject to CORS restritions on the target server.
  */
 add_action(
 	'admin_notices',
-	function () {
-		if ( 'tablepress_import' !== get_current_screen()->id ) {
-			return;
+	static function () {
+		if ( 'tablepress_import' === get_current_screen()->id ) {
+			echo '<div class="notice components-notice is-warning"><div class="components-notice__content"><p><strong>Important notice:</strong><br>Due to how this in-browser demo works behind the scenes, import from URLs might not work for all URLs! It will however work fine on a real WordPress installation!</p></div></div>';
 		}
-		echo '<div class="notice components-notice is-warning"><div class="components-notice__content"><p><strong>Important notice:</strong><br>Due to how this in-browser demo works behind the scenes, import from URLs might not work for all URLs! It will however work fine on a real WordPress installation!</p></div></div>';
 	}
 );
